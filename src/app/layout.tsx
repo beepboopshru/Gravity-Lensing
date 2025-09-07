9186db44ff2ae949306da7fd4953a1ead4ad5e4c
Shreyanshu Jaiswal <Shreyanshu.jais@gmail.com>
2025-09-07 06:55:29 +0000

    Name it to Gravity Lensing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Grav Lens',
+  title: 'Gravity Lensing',
   description: 'An interactive 3D Gravitational Lensing Visualizer',
 };
 
